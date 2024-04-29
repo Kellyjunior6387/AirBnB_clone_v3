485ce3f44a6c59703d22755f3684bb68c638fe56
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-04-29 11:34:07 +0300

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,23 @@
 Endpoint to return status of api
 """
 
+import os
 from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = int(os.getenv('HBNB_API_PORT', 5000))
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """ closes the storage on teardown"""
     storage.close()
+
+cors = CORS(app, resources={r'/*': {'origins': host}})
 
 
 @app.errorhandler(404)
@@ -22,7 +28,4 @@ def not_found(error):
     return make_response({"error": "Not found"}, 404)
 
 if __name__ == "__main__":
-    import os
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
